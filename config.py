19f894e4e3854e2828a38120ecbf188354ac2f3c
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2023-11-03 14:59:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 6743351101)
+OWNER_ID = int(getenv("OWNER_ID", None)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/english_hindi_friends_chatting")
